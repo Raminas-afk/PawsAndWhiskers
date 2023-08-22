f71f5a90ba505a42c311fe7534bc94dd83e7cc90
Raminas <raminasceponis7@gmail.com>
2023-08-22 20:59:58 +0300

    changed celery schedule for testing

--- pawsandwhiskers/celery.py
+++ pawsandwhiskers/celery.py
@@ -14,8 +14,6 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'send-daily-emails': {
         'task': 'main.tasks.send_daily_email_task',
-        'schedule': crontab(hour=12, minute=0),
+        'schedule': crontab(hour=4, minute=0),
     },
 }
-
-# trigger heroku redeployment 3
